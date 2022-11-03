0ffd2e5de9a3d5ab3e2281756826ae7973a037a7
Lucas Frazao <116677214+School-Lucas-Frazao@users.noreply.github.com>
2022-11-03 15:59:57 -0500

    Updates

--- DoublyListsHolder/src/Queue.java
+++ DoublyListsHolder/src/Queue.java
@@ -1,4 +1,20 @@
 
-public class Queue {
-
+public class Queue <T>
+{
+	dNode<T> head = new dNode<T>();
+	dNode<T> tail = new dNode<T>();
+	
+	public Queue()
+	{
+		head = null;
+		tail = null;
+	}
+	
+	public Queue(T t)
+	{
+		head = new dNode<T>(t);
+		tail = head;
+	}
+	
+	
 }
--- DoublyListsHolder/src/dNode.java
+++ DoublyListsHolder/src/dNode.java
@@ -1,4 +1,27 @@
+public class dNode<T> {
 
-public class dNode {
+     T data;
+    dNode<T> next;
+    dNode<T> prev;
 
+    public dNode()
+    {
+    	data = null;
+    	next = null;
+    	prev = null;
+    }
+    
+    public dNode(T data)
+    {
+    	this.data = data;
+    	next = null;
+    	prev = null;
+    }
+ 
+    public dNode(T data, dNode<T> next, dNode<T> prev)
+    {
+    	this.data = data;
+    	this.next = next;
+    	this.next = next;
+    }  
 }
