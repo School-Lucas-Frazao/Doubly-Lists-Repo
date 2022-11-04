2c4e4bb9250fa4a3b82b72d8a42ce15b4dd487ad
Lucas Frazao <116677214+School-Lucas-Frazao@users.noreply.github.com>
2022-11-04 14:38:14 -0500

    Adds the queue method

--- DoublyListsHolder/src/Queue.java
+++ DoublyListsHolder/src/Queue.java
@@ -16,5 +16,47 @@ public class Queue <T>
 		tail = head;
 	}
 	
+	public void enqueue(T value)
+	{
+		dNode<T> New = new dNode<T>();//makes the listnode nw
+		dNode<T> temp = new dNode<T>();//sets temp to listnode new
+		New.data = value;//sets the data in new equal to the parameter
+		temp = head;//sets temp equal to front
+		if( head == null)//if list is empty
+		{
+			head = New;
+			tail = New;
+		}
+		else//if list is not empty
+		{
+			
+			while( temp.next != null)
+			{
+				temp = temp.next;
+			}
+			
+			temp.next = New;
+			temp.prev = temp;
+			tail = temp.next;
+			//temp.next.next = temp;
+		}
+	}
+	
+	
+	
+	public String toString()
+	{
+		String x = "";//will be what is returned
+		dNode<T> temp = new dNode<T>();
+		temp = head;
+		while(temp != tail)
+		{
+			x = x + temp.data + ",";
+			temp = temp.next;
+		}
+		return x;
+		
+	}
+	
 	
 }
---
+++ DoublyListsHolder/src/TestDLists.java
@@ -0,0 +1,46 @@
+/*
+ * LinkedListReplicated.java, Lucas Frazao, Period 7, file will serve to test all methods in
+ * LinkedIntList.java
+ * 
+ */
+
+//import java.io.File;
+//import java.io.IOException;
+
+
+public class TestDLists
+{
+ 
+    public static void main(String s[]) 
+    {
+    	
+		//Students create 2 LinkedIntList objects.
+    	
+    	Queue<Integer> one = new Queue<Integer>();
+    	
+    	
+
+    	
+		
+      	//Test out each of your various methods (add, insert, get, indexOf, remove, size, toString)) 
+      	//using either of the instantiated LinkedIntList objects
+    	
+    	one.enqueue(1);
+    	one.enqueue(2);
+    	one.enqueue(3);
+    	one.enqueue(4);
+    	System.out.println(one);
+    	
+    	
+    	
+    	
+    	
+
+    	
+    	
+    	
+
+    }
+    
+	//Create your LinkedIntList class in another file called "LinkedIntList"
+}
